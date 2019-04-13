1f66d0baaf2c8233fcb8cb3fb73aa492fa55dff3
Erosyen <1383162666@qq.com>
2019-04-13 17:22:55 +0800

    Merge branch 'master' of D:\IdeaProject\oa with conflicts.

---
+++ src/main/java/com/qianfeng/controller/CourseController.java
@@ -0,0 +1,40 @@
+package com.qianfeng.controller;
+
+import com.qianfeng.common.JsonBean;
+import com.qianfeng.entity.TCourse;
+import com.qianfeng.service.CourseService;
+import com.qianfeng.utils.JsonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@ResponseBody
+public class CourseController {
+    @Autowired
+    private CourseService courseService;
+
+    @RequestMapping("/add.do")
+    public JsonBean addCou(TCourse tCourse) {
+        try {
+            courseService.addCourse(tCourse);
+            return JsonUtils.createJsonBean(1, 1,null);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return JsonUtils.createJsonBean(0,0,e.getMessage());
+        }
+
+    }
+    @RequestMapping("/courselist.do")
+    public JsonBean listCou(Model model){
+        List<TCourse> list = courseService.findAll();
+
+
+        return JsonUtils.createJsonBean(1,1,list);
+    }
+}
